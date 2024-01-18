4f95f6dbc047f03c360b05c1c34f5aca38b4c73c
Shaurya Jain <shauryajain236@gmail.com>
2024-01-18 13:15:29 +0530

    chanhge

--- app/src/main/java/com/example/geekyhub/mento_bliss/MainScreen.kt
+++ app/src/main/java/com/example/geekyhub/mento_bliss/MainScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.cardview.widget.CardView
 import com.example.geekyhub.mento_bliss.databinding.ActivityHomeBinding
 
@@ -14,26 +15,26 @@ class MainScreen : AppCompatActivity() {
         setContentView(R.layout.activity_main_screen)
         supportActionBar?.hide()
 
-        val test = findViewById<CardView>(R.id.conduct_test)
+        val test = findViewById<Button>(R.id.conduct_test)
         test.setOnClickListener(){
             val intent= Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
 
-        val playlist = findViewById<CardView>(R.id.playlists)
+        val playlist = findViewById<Button>(R.id.specialist)
         playlist.setOnClickListener(){
             val openUrl = Intent(Intent.ACTION_VIEW)
             openUrl.data = Uri.parse("https://www.google.com/maps/search/psychiatrist+near+me/@28.6825986,77.2657815,11z/data=!3m1!4b1?entry=ttu")
             startActivity(openUrl)
         }
 
-        val vedio = findViewById<CardView>(R.id.reports)
+        val vedio = findViewById<Button>(R.id.reports)
         vedio.setOnClickListener(){
             val intent= Intent(this, Vedios::class.java)
             startActivity(intent)
         }
 
-        val about = findViewById<CardView>(R.id.about_us)
+        val about = findViewById<Button>(R.id.about_us)
         about.setOnClickListener(){
             val intent= Intent(this, About_us::class.java)
             startActivity(intent)
